6d849863c5b748c0e6ccbd0459040b0efba16620
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-11-06 13:39:16 -0500

    Create BcsQBWCBundle.php

---
+++ src/BcsQBWCBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao QBWC
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-qbwc
+ */
+
+namespace Bcs\QBWCBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsQBWCBundle extends Bundle
+{
+}
