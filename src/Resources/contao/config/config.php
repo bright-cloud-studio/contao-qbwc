2d7d99e07fcd623f6ef323278f18cc0b8e8b3f92
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-03 14:13:26 -0500

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,12 @@
 <?php
 
-  
+/**
+* @copyright  Bright Cliud Studio
+* @author     Bright Cloud Studio
+* @package    Contao Quickbooks Web Connector
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/contao-qbwc
+*/
+
+/* Front End modules */
+$GLOBALS['FE_MOD']['gai']['mod_list_services'] = 'Bcs\Module\ModReceiveData';
