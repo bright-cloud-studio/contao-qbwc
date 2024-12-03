3fe744549812847380bc3bcbd4b030a683834c6a
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-03 14:19:20 -0500

    Update modules.php

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -1,4 +1,7 @@
 <?php
 
 /* Back End modules */
-$GLOBALS['TL_LANG']['MOD']['contao_openai']        = array('OpenAI', 'OpenAI Settings');
+$GLOBALS['TL_LANG']['MOD']['contao_openai']            = array('OpenAI', 'OpenAI Settings');
+
+/* Front End modules */
+$GLOBALS['TL_LANG']['FMD']['mod_receive_data']        = array('Quickbooks - Receive Data', 'Add to a page to capture pushed XML data from Quickbooks Web Connector');
