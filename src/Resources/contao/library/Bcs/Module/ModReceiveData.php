776ef84d6cbc0e5032c57bf8b45a72d4c49a0964
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-03 14:15:42 -0500

    Create ModReceiveData.php

---
+++ src/Resources/contao/library/Bcs/Module/ModReceiveData.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+* Bright Cloud Studio's GAI Payment Dashboard
+*
+* Copyright (C) 2024-2025 Bright Cloud Studio
+*
+* @package    bright-cloud-studio/gai-payment-dashboard
+* @link       https://www.brightcloudstudio.com/
+* @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+namespace Bcs\Module;
+
+class ModReceiveData extends \Contao\Module
+{
+  
+}
