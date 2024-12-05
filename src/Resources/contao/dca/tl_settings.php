eec10d83e0548962156c983f62f1090b58f64b9a
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-05 09:36:07 -0500

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,15 +1,1 @@
 <?php
-
-use Contao\Config;
-
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{oepnai_legend}, openai_model, openai_prompt, openai_automatic, openai_extensions;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
-
-
-$GLOBALS['TL_DCA']['tl_settings']['fields'] += [
-    'openai_extensions' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['openai_extensions'],
-        'inputType'         => 'text',
-        'default'           => 'Based on this image, create a search engine optimized alt text (under 15 words)',
-        'eval'              => array('mandatory'=>true, 'tl_class'=>'w50', 'default'=> 'Based on this image, create a search engine optimized alt text (under 15 words)'),
-    ]
-];
