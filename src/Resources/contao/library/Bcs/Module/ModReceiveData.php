b7b490701e32c2c616bba954add2e206c859c794
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-05 10:19:54 -0500

    Update ModReceiveData.php

--- src/Resources/contao/library/Bcs/Module/ModReceiveData.php
+++ src/Resources/contao/library/Bcs/Module/ModReceiveData.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
-* Bright Cloud Studio's GAI Payment Dashboard
+* Bright Cloud Studio's Contao QBWC
 *
 * Copyright (C) 2024-2025 Bright Cloud Studio
 *
-* @package    bright-cloud-studio/gai-payment-dashboard
+* @package    bright-cloud-studio/contao-qbwc
 * @link       https://www.brightcloudstudio.com/
 * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
@@ -14,5 +14,21 @@ namespace Bcs\Module;
 
 class ModReceiveData extends \Contao\Module
 {
-  
+
+
+    // Main function called when the page this is on loads
+    protected function compile()
+	{
+		if (!isset($_GET['keywords']) && Input::post('FORM_SUBMIT') == 'tl_search')
+		{
+			//$_GET['keywords'] = Input::post('keywords');
+		}
+
+        echo "Module: compile()';
+        die();
+        
+	}
+
+
+    
 }
