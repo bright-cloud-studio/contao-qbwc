ef5ddb6ac0f902f1534fd7642093dc24e7380cc7
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-05 12:28:06 -0500

    Create tl_settings.php

---
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['qbwc_legend']             = 'QBWC Settings';
+$GLOBALS['TL_LANG']['tl_settings']['qbwc_username']           = array('Username', 'Enter the Username that was set in QBWC.');
+$GLOBALS['TL_LANG']['tl_settings']['qbwc_password']           = array('Password', 'Enter the Password that was set in QBWC.');
