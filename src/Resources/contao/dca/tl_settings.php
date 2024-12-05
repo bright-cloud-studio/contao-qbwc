df80d2e6594edd62fb0468a0bc0c47a9add04ab4
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-05 11:29:48 -0500

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,1 +1,18 @@
 <?php
+
+// Add our custom fields to the palette
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{qbwc_legend}, qbwc_username, qbwc_password;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+
+// Create those custom fields
+$GLOBALS['TL_DCA']['tl_settings']['fields'] += [
+    'qbwc_username' => [
+        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['qbwc_username'],
+        'inputType'         => 'text',
+        'eval'              => ['mandatory' => 'false', 'tl_class' => 'w50'],
+    ],
+    'qbwc_password' => [
+        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['qbwc_password'],
+        'inputType'         => 'text',
+        'eval'              => ['mandatory' => 'false', 'tl_class' => 'w50'],
+    ]
+];
