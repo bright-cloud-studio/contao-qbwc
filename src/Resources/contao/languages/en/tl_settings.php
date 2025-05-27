3ec89c94eee69a40a578b3fdaa39c6215de2b3bf
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-27 12:06:54 -0400

    Update tl_settings.php

--- src/Resources/contao/languages/en/tl_settings.php
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -3,3 +3,6 @@
 $GLOBALS['TL_LANG']['tl_settings']['qbwc_legend']             = 'QBWC Settings';
 $GLOBALS['TL_LANG']['tl_settings']['qbwc_username']           = array('Username', 'Enter the Username that was set in QBWC.');
 $GLOBALS['TL_LANG']['tl_settings']['qbwc_password']           = array('Password', 'Enter the Password that was set in QBWC.');
+
+$GLOBALS['TL_LANG']['tl_settings']['last_run']                         = array('Last Run Timestamp', 'Each time an update is processed, we set the timestamp here');
+$GLOBALS['TL_LANG']['tl_settings']['minute_notification_threshold']    = array('Notification Threshold in Minutes', 'If this many minutes pass without an update send a notification email to check the connection');
