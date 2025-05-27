5e5a83a31ab99689e32b5821ce4ea7e72567116b
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-27 11:56:43 -0400

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,7 +1,7 @@
 <?php
 
 // Add our custom fields to the palette
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{qbwc_legend}, qbwc_username, qbwc_password, last_run;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{qbwc_legend}, qbwc_username, qbwc_password, last_run, minute_notification_threshold;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 // Create those custom fields
 $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
@@ -17,6 +17,11 @@ $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
     ],
     'last_run' => [
         'label'             => &$GLOBALS['TL_LANG']['tl_settings']['last_run'],
+        'inputType'         => 'text',
+        'eval'              => ['mandatory' => 'false', 'tl_class' => 'w50'],
+    ],
+    'minute_notification_threshold' => [
+        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['minute_notification_threshold'],
         'inputType'         => 'text',
         'eval'              => ['mandatory' => 'false', 'tl_class' => 'w50'],
     ]
