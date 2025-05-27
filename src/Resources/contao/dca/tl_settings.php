aa7f0e3738d56c9c1903dc715fb80ed66bf9fb30
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-27 14:23:17 -0400

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,7 +1,7 @@
 <?php
 
 // Add our custom fields to the palette
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{qbwc_legend}, qbwc_username, qbwc_password, last_run, minute_notification_threshold;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{qbwc_legend}, qbwc_username, qbwc_password;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 // Create those custom fields
 $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
@@ -14,15 +14,5 @@ $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
         'label'             => &$GLOBALS['TL_LANG']['tl_settings']['qbwc_password'],
         'inputType'         => 'text',
         'eval'              => array('mandatory'=>true, 'tl_class'=>'w50')
-    ],
-    'last_run' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['last_run'],
-        'inputType'         => 'text',
-        'eval'              => array('rgxp'=>'datim', 'datepicker'=>true, 'mandatory'=>false, 'tl_class'=>'w50')
-    ],
-    'minute_notification_threshold' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['minute_notification_threshold'],
-        'inputType'         => 'text',
-        'eval'              => array('mandatory'=>false, 'tl_class'=>'w50')
     ]
 ];
