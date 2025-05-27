7057b3bb23a8c2009270d367642751dc32b6f3c9
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-27 14:48:18 -0400

    Update modules.php

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -1,7 +1,11 @@
 <?php
 
 /* Back End modules */
-$GLOBALS['TL_LANG']['MOD']['contao_openai']            = array('OpenAI', 'OpenAI Settings');
+$GLOBALS['TL_LANG']['MOD']['contao_openai']                          = array('OpenAI', 'OpenAI Settings');
 
 /* Front End modules */
-$GLOBALS['TL_LANG']['FMD']['mod_receive_data']        = array('Quickbooks - Receive Data', 'Add to a page to capture pushed XML data from Quickbooks Web Connector');
+$GLOBALS['TL_LANG']['FMD']['mod_receive_data']                       = array('Quickbooks - Receive Data', 'Add to a page to capture pushed XML data from Quickbooks Web Connector');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_module']['last_run']                         = array('Last Run Timestamp', 'Each time an update is processed, we set the timestamp here');
+$GLOBALS['TL_LANG']['tl_module']['minute_notification_threshold']    = array('Notification Threshold in Minutes', 'If this many minutes pass without an update send a notification email to check the connection');
