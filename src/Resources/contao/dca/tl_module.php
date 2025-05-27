2d7e1dfd96dcd4708271f1ecbcfbdf9975e32c18
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-27 14:31:20 -0400

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -4,6 +4,16 @@
 $GLOBALS['TL_DCA']['tl_module']['palettes']['mod_receive_data']	= '{title_legend},name,type; {receive_data_legend}, last_run, minute_notification_threshold;';
 
 /** Fields */
+$GLOBALS['TL_DCA']['tl_module']['fields']['bodyNumberOfActive'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['bodyNumberOfActive'],
+	'default'                 => 4,
+	'exclude'                 => true,
+	'inputType'               => 'text',
+	'eval'                    => array('mandatory'=>true, 'rgxp'=>'natural', 'tl_class'=>'w50'),
+	'sql'                     => "smallint(5) unsigned NOT NULL default '0'"
+);
+
 $GLOBALS['TL_DCA']['tl_module']['fields']['last_run'] = array
 (
 	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['last_run'],
