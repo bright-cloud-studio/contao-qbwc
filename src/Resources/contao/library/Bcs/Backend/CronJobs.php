925e980030119e856fa208bdf3e1ffe93598a4ef
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-27 15:15:06 -0400

    Update CronJobs.php

--- src/Resources/contao/library/Bcs/Backend/CronJobs.php
+++ src/Resources/contao/library/Bcs/Backend/CronJobs.php
@@ -7,8 +7,8 @@ use Contao\System;
 class CronJobs extends System
 {
     
-    // If there are X $days_before the end of the month then send reminder emails to all psychologists
-    public function sendReminderEmails(): void
+    // Runs every hour to see if we are still connected
+    public function checkQuickbooksConnection(): void
     {
         $log = fopen($_SERVER['DOCUMENT_ROOT'] . '/../'.date('m_d_y').'_cron_log.txt', "a+") or die("Unable to open file!");
         fwrite($log, 'Cron Triggered! '. time() .'\n');
