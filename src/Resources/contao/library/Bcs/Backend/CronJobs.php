f6f5d0ef37972a48c6f678a356bd05f94b91bb0d
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-28 08:00:40 -0400

    Update CronJobs.php

--- src/Resources/contao/library/Bcs/Backend/CronJobs.php
+++ src/Resources/contao/library/Bcs/Backend/CronJobs.php
@@ -2,6 +2,7 @@
 
 namespace Bcs\Backend;
 
+use Contao\ModuleModel;
 use Contao\System;
 
 class CronJobs extends System
@@ -11,8 +12,31 @@ class CronJobs extends System
     public function checkQuickbooksConnection(): void
     {
         $log = fopen($_SERVER['DOCUMENT_ROOT'] . '/../'.date('m_d_y').'_cron_log.txt', "a+") or die("Unable to open file!");
-        fwrite($log, 'Cron Triggered! '. time() .'\n');
-        fclose($log);   
+        fwrite($log, 'Cron Triggered! '. time() . PHP_EOL);
+        fclose($log);
+        
+        
+        
+        // Get the minute threshold
+        $mod = ModuleModel::findOneBy('minute_notification_threshold > ?', 0);
+        if($mod) {
+            
+            fwrite($log, print_r($mod, true). PHP_EOL);
+            
+        }
+        
+        // Get the 'last_run'
+        
+        // See how many minutes have elapsed between 'last_run' and now
+        
+        // if greater minute threshold, send notification email
+        
+        
+        
+        
+        
+        
+        
     }
 
 
